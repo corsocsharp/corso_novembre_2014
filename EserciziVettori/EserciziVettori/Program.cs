1153d63195295914ed17c2d340dbcf14622b2ce5
corsocsharp <corsocsharp@outlook.com>
2014-11-11 15:30:53 +0100

    esempi array/liste

---
+++ EserciziVettori/EserciziVettori/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EserciziVettori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> dati = new List<string>();
+
+            //bool continua = true;
+            //while (continua) {
+            //    string testo = LeggiTesto("Inserisci un testo");
+            //    if (testo == "")
+            //    {
+            //        continua = false;
+            //    }
+            //    else {
+            //        dati.Add(testo);
+            //    }
+            //}
+
+
+            //string testo1;
+            //do{
+            //    testo1 =  LeggiTesto("Inserisci un testo");
+            //    if (testo1 != "") {
+            //        dati.Add(testo1);
+            //    }
+            //}while(testo1 != "");
+
+
+            while (true) {
+                string testo2 = LeggiTesto("Inserisci un testo");
+
+                if (String.IsNullOrEmpty(testo2)){
+                    break;
+                }
+                
+                dati.Add(testo2);
+            }
+
+            int lungMax = 0;
+            string maxString = "";
+            foreach (string testo in dati)
+            {
+                if (testo.Length > lungMax)
+                {
+                    lungMax = testo.Length;
+                    maxString = testo;
+                }
+            }
+
+            Console.WriteLine("Testo di lungh. massima: '{0}' ( {1} ) ",
+                        maxString, lungMax);
+
+
+
+            Console.ReadKey();
+        }
+
+        private static void ElaboraNumeroParametriFisso()
+        {
+            string[] dati = new string[3];
+
+            for (int i = 0; i < dati.Length; i++)
+            {
+                dati[i] = LeggiTesto("Inserisci un testo");
+            }
+
+
+            int lungMax = 0;
+            string maxString = "";
+            foreach (string testo in dati)
+            {
+                if (testo.Length > lungMax)
+                {
+                    lungMax = testo.Length;
+                    maxString = testo;
+                }
+            }
+
+            //Console.WriteLine("Testo di lunghezza massima: '"
+            //        + maxString + "' (" + lungMax + ")");
+
+            //string output =
+            //    String.Format("Testo di lungh. massima: '{0}' ( {1} ) ",
+            //            maxString, lungMax);
+            //Console.WriteLine(output);
+
+            Console.WriteLine("Testo di lungh. massima: '{0}' ( {1} ) ",
+                        maxString, lungMax);
+        }
+
+        private static string LeggiTesto(string messaggio)
+        {
+            Console.WriteLine(messaggio);
+            Console.Write("> ");
+            string testo = Console.ReadLine();
+            return testo;
+        }
+    }
+}
---
+++ EserciziVettori/StatisticheNumeri/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StatisticheNumeri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeri = new List<int>();
+
+            while (true) {
+                string testo = LeggiTesto("Inserisci un numero");
+             
+                if (String.IsNullOrEmpty(testo)) break;
+
+                int valore = Convert.ToInt32(testo);
+
+                //if (valore == 0) break;
+
+                numeri.Add(valore);
+            }
+
+            //int min = numeri[0];
+            //int max = numeri[0];
+            int min = Int32.MaxValue;
+            int max = Int32.MinValue;
+            int numPositivi = 0;
+            double somma = 0;
+
+            foreach (int valore in numeri) {
+                
+                somma += valore;
+
+                if (valore > 0) numPositivi++;
+
+                if (valore > max) max = valore;
+
+                if (valore < min) min = valore;
+            }
+
+            double media = somma / numeri.Count;
+
+            Console.WriteLine("Numeri: " + numeri.Count);
+            Console.WriteLine("Somma: " + somma);
+            Console.WriteLine("Media: " + media);
+            Console.WriteLine("Min: " + min);
+            Console.WriteLine("Max: " + max);
+
+            Console.ReadKey();
+        }
+
+        private static string LeggiTesto(string messaggio)
+        {
+            Console.WriteLine(messaggio);
+            Console.Write("> ");
+            string testo = Console.ReadLine();
+            return testo;
+        }
+    }
+}
