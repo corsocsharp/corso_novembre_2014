0a2efa65c04b363a5d0bed8f7f21424ede47e5f5
corsocsharp <corsocsharp@outlook.com>
2014-11-12 16:10:18 +0100

    mvc inizio

---
+++ WebBase1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Prova()
+        {
+            return View();
+        }
+
+
+
+        public string Ora()
+        {
+            return "Ora: " + DateTime.Now;
+        }
+
+        public int Somma(int val1 = 0 , int val2 = 0) {
+
+            return val1 + val2;
+        }
+
+    }
+}
