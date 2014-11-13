476411dc151bb4c6572dea59d40ad452709d3b77
corsocsharp <corsocsharp@outlook.com>
2014-11-13 16:37:50 +0100

    triangolo e tavola pitagorica

--- MvcApplication2/MvcApplication2/Controllers/DisegnaController.cs
+++ MvcApplication2/MvcApplication2/Controllers/DisegnaController.cs
@@ -22,15 +22,32 @@ namespace MvcApplication2.Controllers
 
         }
 
-        public ActionResult Quadrato(int lato = 10, string carattere = "*")
+        public ActionResult Quadrato(string azione, int lato = 10,
+                                                string carattere = "*")
         {
+
+            if (String.IsNullOrEmpty(azione)) {
+                azione = "Quadrato";
+            }
 
             ViewBag.Lato = lato;
             ViewBag.Carattere = carattere;
+            ViewBag.Azione = azione;
 
             return View();
 
         }
 
+        public ActionResult Rettangolo(int latoA = 10, int latoB = 10, 
+                        string carattere = "*")
+        {
+
+            ViewBag.LatoA = latoA;
+            ViewBag.LatoB = latoB;
+            ViewBag.Carattere = carattere;
+
+            return View();
+
+        }
     }
 }
