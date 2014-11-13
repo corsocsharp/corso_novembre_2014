a9a4d22d94ee62f25930ce4ffde9874b4a992ed6
corsocsharp <corsocsharp@outlook.com>
2014-11-13 13:47:58 +0100

    somma e quadrato

---
+++ MvcApplication2/MvcApplication2/Controllers/DisegnaController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class DisegnaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("righe");
+        }
+
+        public ActionResult Righe(int lunghezza = 10)
+        {
+
+            ViewBag.Lunghezza = lunghezza;
+
+            return View();
+
+        }
+
+        public ActionResult Quadrato(int lato = 10, string carattere = "*")
+        {
+
+            ViewBag.Lato = lato;
+            ViewBag.Carattere = carattere;
+
+            return View();
+
+        }
+
+    }
+}
---
+++ MvcApplication2/MvcApplication2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Titolo in action";
+            return View();
+        }
+
+        public string FaiCose() {
+            return "ciao";
+        }
+
+        public ActionResult Somma(int val1 = 0, int val2 = 0) {
+
+            int somma = val1 + val2;
+
+            ViewBag.Valore1 = val1;
+            ViewBag.Valore2 = val2;
+            ViewBag.Risultato = somma;
+
+            return View();
+
+        }
+
+    }
+}
